b9793d35759e0f7e3ab9f981d79351625284b92c
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2023-05-13 07:12:22 +0800

    Update ossn_com.php

--- SuperImageSizeSaver/ossn_com.php
+++ SuperImageSizeSaver/ossn_com.php
@@ -3,7 +3,7 @@
  * Open Source Social Network
  *
  * @package   (openteknik.com).ossn
- * @author    TheDoggybrad and its original authors
+ * @author    TheDoggybrad and OSSN Core Team
  * @copyright (C) OpenTeknik LLC
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
